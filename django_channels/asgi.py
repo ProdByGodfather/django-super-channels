0de55fafee3167e71ce51282e1c05ae4ff518db0
GodFather <mahdighasemiabari84@gmail.com>
2023-12-18 23:25:01 +0330

    Fix Errors and create websocket connection

--- django_channels/asgi.py
+++ django_channels/asgi.py
@@ -15,7 +15,7 @@ from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
