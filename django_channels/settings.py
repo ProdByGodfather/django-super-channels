c3ab7d3f56bbb5e1ed2e4381b916199e3d267696
GodFather <mahdighasemiabari84@gmail.com>
2023-12-19 21:39:32 +0330

    Config Redis on project

--- django_channels/settings.py
+++ django_channels/settings.py
@@ -27,11 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
+
 
 # Application definition
 
