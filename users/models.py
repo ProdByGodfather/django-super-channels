44eeb660812033e84ecdc65c5daf3695080aad5a
GodFather <mahdighasemiabari84@gmail.com>
2023-12-28 16:35:52 +0330

    Make Private User Model

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    # image = models.ImageField(upload_to='users/%Y/%m', blank=True,null=True)
+    pass
