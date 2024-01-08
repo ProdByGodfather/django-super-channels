e6ff54cab6fd92cc713e1a30c2738d72dceff741
Mahdi Ghasemi <106802820+ProdByGodfather@users.noreply.github.com>
2024-01-08 15:36:20 +0330

    `v2.3.0` - make user profiles in chat room and fix bugs

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name='chat/login.html'), name='login'),
     # register have a view
     path('register/', views.RegisterView.as_view(), name='register'),
-    path("profile/",views.Profile.as_view(),name='profile')
+    path("profile/",views.Profile.as_view(),name='profile'),
+    path("profile/<str:username>/",views.user_profile,name='user_profile')
 ]
